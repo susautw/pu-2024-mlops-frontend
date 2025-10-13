c2a912a0fae985822849666d3c88749b5d2d46e9
susautw <susautw@gmail.com>
2025-10-13 17:32:14 +0000

    feat: implement WorkerListItem component and test it in WorkersView

---
+++ src/components/worker/WorkerListItem.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+/**
+ * WorkerListItem - A component representing a single worker item in a list.
+ * This component is intended to display information about a worker, such as its name,
+ * status, and other relevant details. It can be used within a list to show multiple workers.
+ */
+export default {}
+</script>
+<template>
+  <div class="flex py-2 px-4 gap-8 b-1 b-white/20 hover:bg-white/40">
+    <span class="font-bold">{{ item.id }}</span>
+    <span>
+      {{ item.task_type }}
+      <sub>{{ item.version }}</sub>
+    </span>
+  </div>
+</template>
+<script lang="ts" setup>
+import type { WorkerSummary } from '@/libs/apis/models'
+
+defineProps<{
+  item: WorkerSummary
+}>()
+</script>
--- src/views/WorkersView.vue
+++ src/views/WorkersView.vue
@@ -1,7 +1,32 @@
 <template>
-  <PageFrame />
+  <PageFrame>
+    <template #actions>
+      <div class="grow flex gap-2 items-center">
+        <div class="grow" />
+        <button
+          class="pi pi-refresh p-2 bg-white/20 hover:bg-blue active:bg-blue/80 rounded-2 transition-colors-150"
+        />
+      </div>
+    </template>
+    <template #default>
+      <WorkerListItem :item="testItem" />
+    </template>
+  </PageFrame>
 </template>
 
 <script setup lang="ts">
 import PageFrame from '@/components/PageFrame.vue'
+import WorkerListItem from '@/components/worker/WorkerListItem.vue'
+import type { WorkerSummary } from '@/libs/apis/models'
+
+const testItem: WorkerSummary = {
+  id: 'worker-123',
+  task_type: 'data-processing',
+  version: '1.0.0',
+  created_at: new Date(),
+  has_task: false,
+  healthy: true,
+  reported_at: new Date(),
+  joined_at: new Date(),
+}
 </script>
