a11bac7c4f894de53fab01f1d370db07414e4412
waterspirit743 <yoyo301181@gmail.com>
2025-10-16 04:13:18 +0800

    feat: implement TaskListitem component and integrate it into TasksView for task display

---
+++ src/components/task/TaskListitem.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+
+
+
+
+
+export default {}
+</script>
+<template>
+ <div
+    class="flex py-2 px-4 gap-8 b-1 b-white/20 hover:bg-white/21 cursor-pointer active:bg-white/22 select-none"
+  >
+    <span
+      class="font-bold w-80 overflow-clip text-ellipsis whitespace-nowrap"
+      v-tooltip.top="{
+        value: item.task_id,
+        showDelay: 700,
+        hideDelay: 0,
+      }"
+      >
+      {{ item.task_id }}
+    </span>
+    <span class="w-50 overflow-clip text-ellipsis whitespace-nowrap"
+    v-tooltip.top="{
+        value: item.task_type + ' ' + item.version,
+        showDelay: 500,
+        hideDelay: 0,
+      }"
+    >
+      {{ item.task_type }}
+      <sub>{{ item.version }}</sub>
+    </span>
+
+  </div>
+</template>
+<script lang="ts" setup>
+import type { TrainingTask ,} from '@/libs/apis/models'
+
+
+defineProps<{
+  item: TrainingTask
+}>()
+</script>
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -11,14 +11,79 @@
         />
       </div>
     </template>
-    <template #default> Tasks </template>
-  </PageFrame>
+    <template #default>
+      <div class="flex flex-col h-full">
+        <div class="table-header flex py-2 px-4 gap-8 gap-2 font-bold text-dark bg-amber/90">
+          <span class="font-bold w-80">Task ID</span>
+          <span class="w-50">Task Type</span>
+      </div>
+      <div v-if="loading" class="grow flex items-center justify-center text-white/50">
+         <span class="pi pi-spin pi-spinner !text-10 animate-fade-in animate-duration-1s" />
+      </div>
+      <div
+          v-if="!loading && tasks.length === 0"
+          class="grow flex items-center justify-center text-white/50"
+        >
+                    No tasks found.
+        </div>
+    <template v-if="!loading && tasks.length > 0">
+      <TaskListitem
+      v-for="task in tasks"
+      :key="task.task_id"
+      :item="task"
+      @click="handleTaskItemClick(task)"
+      />
+      <ProgressBar
+      v-for="task_status in task_status"
+      :value="task_status.progress"/>
+    </template>
+  </div>
+</template>
+</PageFrame>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import PageFrame from '@/components/PageFrame.vue'
 import SearchBar from '@/components/SearchBar.vue'
 import IconButton from '@/components/IconButton.vue'
+import TaskListitem from '@/components/task/TaskListitem.vue'
+import { getTrainingTasks  } from '@/libs/apis/apis'
+import { type TrainingStatus, type TrainingTask } from '@/libs/apis/models'
+import { useRouter } from 'vue-router'
+import { delay } from '@/libs/utils'
+
+
+const router = useRouter()
+const task_status = ref<TrainingStatus[]>([])
+const tasks = ref<TrainingTask[]>([])
+const loading = ref(true)
+
+async function loadTasks(){
+  loading.value=true
+  delay(500)
+  const remoteTasks= await getTrainingTasks()
+  loading.value=false
+  delay(500)
+  tasks.value=remoteTasks
+}
+async function loadtaskstatus() {
+  loading.value = true
+//need help
+  loading.value = false
+}
+
+
+function handleTaskItemClick(worker: TrainingTask) {
+  // Navigate to worker detail view
+  router.push({ name: 'worker-detail', params: { id: worker.task_id } })
+}
+
+onMounted(async()=>{
+  await loadTasks()
+  await loadtaskstatus
+  loading.value=false
+})
+
 
 const search = ref('')
 </script>
