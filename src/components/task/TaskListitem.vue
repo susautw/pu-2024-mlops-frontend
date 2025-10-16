1ee41ecce9942e2c3383c86e8bf75471ceab2580
susautw <susautw@gmail.com>
2025-10-16 15:18:10 +0000

    format code

--- src/components/task/TaskListitem.vue
+++ src/components/task/TaskListitem.vue
@@ -1,13 +1,8 @@
 <script lang="ts">
-
-
-
-
-
 export default {}
 </script>
 <template>
- <div
+  <div
     class="flex py-2 px-4 gap-8 b-1 b-white/20 hover:bg-white/21 cursor-pointer active:bg-white/22 select-none"
   >
     <span
@@ -17,11 +12,12 @@ export default {}
         showDelay: 700,
         hideDelay: 0,
       }"
-      >
+    >
       {{ item.task_id }}
     </span>
-    <span class="w-50 overflow-clip text-ellipsis whitespace-nowrap"
-    v-tooltip.top="{
+    <span
+      class="w-50 overflow-clip text-ellipsis whitespace-nowrap"
+      v-tooltip.top="{
         value: item.task_type + ' ' + item.version,
         showDelay: 500,
         hideDelay: 0,
@@ -30,12 +26,10 @@ export default {}
       {{ item.task_type }}
       <sub>{{ item.version }}</sub>
     </span>
-
   </div>
 </template>
 <script lang="ts" setup>
-import type { TrainingTask ,} from '@/libs/apis/models'
-
+import type { TrainingTask } from '@/libs/apis/models'
 
 defineProps<{
   item: TrainingTask
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -16,30 +16,28 @@
         <div class="table-header flex py-2 px-4 gap-8 gap-2 font-bold text-dark bg-amber/90">
           <span class="font-bold w-80">Task ID</span>
           <span class="w-50">Task Type</span>
-      </div>
-      <div v-if="loading" class="grow flex items-center justify-center text-white/50">
-         <span class="pi pi-spin pi-spinner !text-10 animate-fade-in animate-duration-1s" />
-      </div>
-      <div
+        </div>
+        <div v-if="loading" class="grow flex items-center justify-center text-white/50">
+          <span class="pi pi-spin pi-spinner !text-10 animate-fade-in animate-duration-1s" />
+        </div>
+        <div
           v-if="!loading && tasks.length === 0"
           class="grow flex items-center justify-center text-white/50"
         >
-                    No tasks found.
+          No tasks found.
         </div>
-    <template v-if="!loading && tasks.length > 0">
-      <TaskListitem
-      v-for="task in tasks"
-      :key="task.task_id"
-      :item="task"
-      @click="handleTaskItemClick(task)"
-      />
-      <ProgressBar
-      v-for="task_status in task_status"
-      :value="task_status.progress"/>
+        <template v-if="!loading && tasks.length > 0">
+          <TaskListitem
+            v-for="task in tasks"
+            :key="task.task_id"
+            :item="task"
+            @click="handleTaskItemClick(task)"
+          />
+          <ProgressBar v-for="task_status in task_status" :value="task_status.progress" />
+        </template>
+      </div>
     </template>
-  </div>
-</template>
-</PageFrame>
+  </PageFrame>
 </template>
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
@@ -47,43 +45,40 @@ import PageFrame from '@/components/PageFrame.vue'
 import SearchBar from '@/components/SearchBar.vue'
 import IconButton from '@/components/IconButton.vue'
 import TaskListitem from '@/components/task/TaskListitem.vue'
-import { getTrainingTasks  } from '@/libs/apis/apis'
+import { getTrainingTasks } from '@/libs/apis/apis'
 import { type TrainingStatus, type TrainingTask } from '@/libs/apis/models'
 import { useRouter } from 'vue-router'
 import { delay } from '@/libs/utils'
-
 
 const router = useRouter()
 const task_status = ref<TrainingStatus[]>([])
 const tasks = ref<TrainingTask[]>([])
 const loading = ref(true)
 
-async function loadTasks(){
-  loading.value=true
+async function loadTasks() {
+  loading.value = true
   delay(500)
-  const remoteTasks= await getTrainingTasks()
-  loading.value=false
+  const remoteTasks = await getTrainingTasks()
+  loading.value = false
   delay(500)
-  tasks.value=remoteTasks
+  tasks.value = remoteTasks
 }
 async function loadtaskstatus() {
   loading.value = true
-//need help
+  //need help
   loading.value = false
 }
-
 
 function handleTaskItemClick(worker: TrainingTask) {
   // Navigate to worker detail view
   router.push({ name: 'worker-detail', params: { id: worker.task_id } })
 }
 
-onMounted(async()=>{
+onMounted(async () => {
   await loadTasks()
   await loadtaskstatus
-  loading.value=false
+  loading.value = false
 })
-
 
 const search = ref('')
 </script>
